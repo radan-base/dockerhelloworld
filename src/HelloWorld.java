3dc9c284eb230a89be7712ba82338f730741db82
radan-base <radan.base@gmail.com>
2017-04-05 03:18:05 -0600

    another change

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld
 {
 	public static void main (String [] args) {
-		System.out.println("hello world" + String.valueOf(new java.util.Date()));
+		System.out.println("hello world on " + String.valueOf(new java.util.Date()));
 	}
 }
